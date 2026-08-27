58e5d36b3a0c4322f365fd7b1c3b4fe41b020a23
JaimyBM <jaimy.bouwman@edu-kw1c.nl>
2026-08-27 12:28:24 +0200

    Stylesheet toegevoegd aan index.php

--- workshops/index.php
+++ workshops/index.php
@@ -4,3 +4,28 @@
      * Eerste PHP bestand
      */
 ?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Homepage</title>
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+    <body>
+        <?php
+            echo "<h1>Homepage eenvoudige database</h1>";
+
+            echo "<p>test</p>";
+
+            //Variabelen
+            $schoolname = "KW1C";
+            echo "Mijn school = $schoolname";
+
+            echo 6 * 6;
+        ?>
+    </body>
+</html>
